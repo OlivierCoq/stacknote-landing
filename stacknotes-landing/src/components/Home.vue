4fa568d98b991d8dafd4324e1a5629b3ff2edd44
Olivier <coq.olivier36@gmail.com>
2021-02-15 16:07:58 -0500

    updated build

---
+++ stacknotes-landing/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="opacity: 0" :style="{opacity: opacity}">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Home',
+    components: {},
+    data() {
+      return {
+        opacity: 1,
+      }
+    },
+    created() {},
+    methods: {}
+  }
+</script>
+<style lang="scss">
+  @import "../sass/main.scss";
+</style>
